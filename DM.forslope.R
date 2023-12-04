ba0327ee0e338065b392210888252cd96927b42c
christian-rummey <christian.rummey@gmail.com>
2023-12-04 11:36:23 +0100

    DEC4#1

--- DM.forslope.R
+++ DM.forslope.R
@@ -12,11 +12,8 @@ fars. %<>%
   group_by(study, sjid, paramcd) %>% 
   mutate( time. = as.numeric( adt-min(adt) ) / 365.25 ) %>% 
   select(-c(avisit, avisitn, hpf, fpf)) %>% ungroup %>%  
-  left_join(.dd('demo') %>% select(sjid, site, sev.o, aoo, gaa1, pm)) %>% 
-  mutate(gaa100 = gaa1/100) %>% 
-  mutate ( pm.grp = ifelse(pm     %in% c('G130V', 'I154F'), 'miss', pm)) %>% 
-  mutate ( pm.grp = ifelse(pm.grp %in% c('0', 'miss'), pm.grp, 'null' ) ) %>% 
-  mutate ( pm.grp = ifelse(pm.grp == '0', 'homoz', pm.grp ) )
+  left_join(.dd('demo') %>% select(study, sjid, site, sev.o, aoo, gaa1, pm)) %>% 
+  mutate(gaa100 = gaa1/100)
 
 bl <- fars. %>% 
   group_by(study, sjid, paramcd) %>% 
@@ -25,7 +22,7 @@ bl <- fars. %>%
 
 fars. %<>%
   left_join(bl) %>% 
-  select(c(study, site, sjid, sev.o, aoo, gaa100, pm, pm.grp, adt, time., paramcd, bl, aval, amb, phase.n, dupline))
+  select(c(study, site, sjid, sev.o, aoo, gaa100, pm, adt, time., paramcd, bl, aval, amb, phase.n, dupline))
 
 fars. %>% 
   saveRDS('DATA derived/fars.forslope.rds')
